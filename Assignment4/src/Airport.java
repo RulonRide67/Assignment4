abd89c1a6bb1036e111e6e8a8664332eca49ab9b
RulonRide67 <armismadiar20@gmail.com>
2025-02-08 13:02:06 +0500

    Update Airport.java

--- Assignment4/src/Airport.java
+++ Assignment4/src/Airport.java
@@ -1,41 +1,51 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Airport{
+public class Airport {
     private String name;
-    ArrayList<Aircraft> aircraft_vehicles= new ArrayList<Aircraft>();
+    private List<Aircraft> aircraftVehicles;
 
     public Airport(String name) {
         this.name = name;
+        this.aircraftVehicles = new ArrayList<>();
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String newname) {
-        this.name = newname;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Aircraft> getAircraftVehicles() {
+        return aircraftVehicles;
+    }
+
+    public void addAircraft(Aircraft aircraft) {
+        getAircraftVehicles().add(aircraft);
+        aircraft.setAirport(this);
+    }
+
+    public void removeAircraft(Aircraft aircraft) {
+        getAircraftVehicles().remove(aircraft);
+        aircraft.setAirport(null);
     }
 
     @Override
     public String toString() {
-        List<String> name_aircrafts= new ArrayList<>();
-        for (Aircraft aircraft : aircraft_vehicles) {
-            name_aircrafts.add(aircraft.getName());
-        }
-        return "Airoport: "+this.name + ", Aircrafts: " + name_aircrafts;
+        return "Airport: " + getName() + ", Aircrafts: " + getAircraftVehicles();
     }
 
-    public void add_aircraft( Aircraft aircraft){
-        aircraft_vehicles.add(aircraft);
-        System.out.println(aircraft.getName() + " added to the " + getName());
-        aircraft.In_which_airport=getName();
-    }
-    public void remove_aircraft(Aircraft aircraft){
-        if (aircraft_vehicles.remove(aircraft)){
-            System.out.println(aircraft.getName() + " removed from the " + getName());
-        }else{
-            System.out.println(aircraft.getName() + " is not founded in " + getName());
+    public Passenger searchPassenger(String aircraftName, int seatNumber) {
+        for (Aircraft aircraft : getAircraftVehicles()) {
+            if (aircraft.getName().equalsIgnoreCase(aircraftName)) {
+                for (Passenger passenger : aircraft.getPassengers()) {
+                    if (passenger.getSeatNumber() == seatNumber) {
+                        return passenger;
+                    }
+                }
+            }
         }
-        aircraft.In_which_airport=null;
+        return null;
     }
 }
